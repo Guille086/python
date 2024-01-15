f26cc0a5407892390168cc47b9912cfbd52a777f
Guille086 <156713115+Guille086@users.noreply.github.com>
2024-01-15 17:03:34 -0300

    Add files via upload
    Ejercicios iniciales de python

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+#suma de dos numeros
+numero_x = float(input("Ingrese un número: "))
+numero_y = float(input("Ingrese un número: "))
+suma = numero_x + numero_y
+
+print("\nla suma de los números es: ", suma)
---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+peso = float(input("Peso (kg): "))
+altura = float(input("Altura (m): "))
+IMC = peso / (altura**2)
+print("\nSu Índice de Masa Corporal es:", IMC)
