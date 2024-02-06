bb3f0f524e286dcccbdfcca7dbb79192cb6df8f1
Guille086 <156713115+Guille086@users.noreply.github.com>
2024-02-06 15:34:26 -0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,27 +1,47 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
-import datetime
+from django.http import HttpResponse
+from .models import Cancion, Usuario, Genero
 
-lista = [12, 45, 78]
-def consultar(request,num):
-    if num in lista:
-        mensaje = "Existe en la lista"
-    else:
-        mensaje = "No existe en la lista"
+# Create your views here.
+def inicio(request):
+    lista_cancion = Cancion.objects.all()
+    dicc = {"canciones":lista_cancion,"saludo":"Holaaaa"}
+    return render(request,"index.html",dicc)
 
-    return HttpResponse(mensaje)
+def login(request):
+    return render(request,"login.html")
 
-def fecha_hora(request):
-    now = datetime.datetime.now()
-    html = "<h1>Hoy es %s.</h1>" % now
-    return HttpResponse(html)
+def autenticar(request):
+    if request.method == 'POST':
+        usuario = request.POST['usuario']
+        password = request.POST['password']
 
-def plantilla_index(request):
-    #cargador de plantilla> loader
-    plantilla = loader.get_template('index.html')
-    pr = plantilla.render()
-    return HttpResponse(pr)
+        if usuario in Usuario.objects.values_list('nickname', flat=True) and password in Usuario.objects.values_list('password', flat=True):
+            usuario_autenticado = Usuario.objects.values('nickname','nombre_apellido','email').distinct().filter(nickname = usuario)
+            lista_cancion = Cancion.objects.filter(usuario_id = usuario)
+            return render(request,"index.html",{'usuario':usuario_autenticado, 'canciones':lista_cancion})
+        else:
+            return render(request,"login.html")
+        
+def agregar_cancion(request):
+    if request.method == 'POST':
+        autor = request.POST['autor']
+        titulo = request.POST['titulo']
+        genero = request.POST['genero']
+        usuario_nick = request.POST['user']
 
-def plantilla_login(request):
-    return render(request,'login.html')
+        #Crear un objeto del tipo usuario
+        usu = Usuario.objects.get(nickname = usuario_nick)
+        gen = Genero.objects.get(id = genero)
+
+        #Crear un objeto del tipo cancion
+        cancion = Cancion()
+        cancion.autor = autor
+        cancion.titulo = titulo
+        cancion.genero = gen
+        cancion.usuario = usu
+
+        cancion.save()
+        usuario_autenticado = Usuario.objects.values('nickname','nombre_apellido','email').distinct().filter(nickname = usuario_nick)
+        lista_cancion = Cancion.objects.filter(usuario_id=usuario_nick)
+        return render(request,"index.html",{'usuario':usuario_autenticado, 'canciones':lista_cancion})
